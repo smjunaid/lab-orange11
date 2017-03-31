e8407471f6fc1334e0520005647038a1b63e505e
smjunaid <junaidanis101@gmail.com>
2017-03-31 17:33:18 +0500

    lab class 4/2/2017

---
+++ app.js
@@ -0,0 +1,233 @@
+// alert('junaid');
+
+// var cities = ['karachi', 'Islamabad', 'Lahore']
+// console.log(cities[2]);
+
+// console.log(cities);
+
+// cities[2] = 'quetta';
+
+// console.log(cities)
+
+
+
+
+
+
+// var name = prompt('type your name');
+// alert('  hi  ' + name)
+
+
+
+
+
+// var array = [];
+// array[2] = 'haider';
+// array[4] = 'ali';
+// console.log(array);
+// console.log(array.length);
+
+
+
+
+// var cities = ['karachi', 'Islamabad', 'Lahore'];
+// cities.pop();
+// console.log(cities);
+
+
+
+
+
+// var qualifications = ['matric', 'intermediate', 'typingtutorial', 'autocad'];
+// qualifications.pop();
+// console.log(qualifications);
+
+
+
+
+
+
+
+// var cartoons = ['Tom and jerry', 'popay', 'moto patlu', 'opgy', 'nemo', 'simba'];
+
+// for (var i = 0; i <= cartoons.length; i++) {
+//     con
+// }
+// console.log(cartoons);
+
+
+// var qualifications = ['matric', 'intermediate', 'typingtutorial', 'autocad'];
+// qualifications.push('phd');
+// console.log(qualifications);
+
+
+
+
+
+// var qualifications = ['matric', 'intermediate', 'typingtutorial', 'autocad'];
+// qualifications.shift();
+// console.log(qualifications);
+
+
+
+
+
+
+
+
+
+// var qualifications = ['matric', 'intermediate', 'typingtutorial', 'autocad'];
+// qualifications.unshift('web designing');
+// console.log(qualifications);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var cartoons = ['Tom and jerry', 'popay', 'jungle', 'oogy', 'nemo', 'simba'];
+
+// console.log(cartoons);
+
+// console.log(cartoons.length);
+
+
+// document.write('1) ' + cartoons[0] + '<br>');
+// document.write('2) ' + cartoons[1] + '<br>');
+// document.write('3) ' + cartoons[2] + '<br>');
+// document.write('4) ' + cartoons[3] + '<br>');
+// document.write('5) ' + cartoons[4] + '<br>');
+// document.write('6) ' + cartoons[5] + '<br>');
+// document.write('7) ' + cartoons[6] + '<br>');
+// document.write('There are ' + cartoons.length + ' in 2017');
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var fruits = ['apple', 'banana', 'graps', 'orange', 'watermillon', 'peach'];
+// fruits.splice(3);
+// console.log(fruits);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var fruits = ['apple', 'banana', 'graps', 'orange', 'watermillon', 'peach'];
+// fruits.splice(2, 0, 'mango', 'stawbery');
+// console.log(fruits);
+
+
+
+
+
+// var studentnames = ['Najam', 'junaid', 'owais', 'qadeer', 'ali', 'akram', 'ismaile', 'bilal', 'ilyas', 'zohaib'];
+// studentnames.splice(prompt("type index number"), 1  , prompt("type new name"));
+// console.log(studentnames);
+
+
+
+
+
+
+
+
+
+
+
+// var studentnames = ['Najam', 'junaid', 'owais', 'qadeer', 'ali', 'akram', 'ismaile', 'bilal', 'ilyas', 'zohaib'];
+// console.log(studentnames.length);
+// console.log(students);
+
+// var a = +prompt('enter index number');
+// var b = prompt('enter replacing valre');
+// if (a > 10 || a < 0 || isNaN(a) {
+//         console.log('something went wrong');
+//     } 
+// else {
+//         students.splice(a, 1, b);
+//         sonsole.log(studentnames);
+//     }
+
+// shift    => start
+// pop      => last
+// Push     =>
+// unshift  =>
+
+
+// splice   => 'update to that array '
+// slice    =>copy
+
+
+
+
+
+
+// var fruits = ['apple', 'banana', 'graps', 'orange', 'watermillon', 'peach'];
+// var favouriteFurits = fruits.slice(1, 4);
+// console.log(favouriteFurits);
+
+
+
+
+
+
+
+
+
+
+
+// for (var i = 0; i < 10; i++) {
+//     console.log('fire bullet');
+
+// }
+
+
+
+for (var i = 0; i <= 100; i++) {
+    console.log('i');
+
+}
+
+
+
+
+
+
+
+
+for (var i = 0; i <= 100; i++) {
+    console.log('i');
+
+}
